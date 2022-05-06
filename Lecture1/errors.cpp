50916efe69baebb8f980cda3c8e37fc5bd38e8a6
Tommaso Corato <tommaso.corato@glovoapp.com>
2022-05-06 22:22:09 +0200

    initial commits

---
+++ Lecture1/errors.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// This coulde would cause a compliation error because there 
+//is no semicolon at the end
+int main(){
+
+    std::cout << "Hello World!" << std::endl
+    return 0;
+}
+
