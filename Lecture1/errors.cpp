939d52d6919002d24cae5a9eb97a11301bffcf62
Tommaso Corato <tommaso.corato@glovoapp.com>
2022-05-06 22:26:00 +0200

    added errors.cpp

--- Lecture1/errors.cpp
+++ Lecture1/errors.cpp
@@ -2,9 +2,17 @@
 
 // This coulde would cause a compliation error because there 
 //is no semicolon at the end
+/*
 int main(){
 
     std::cout << "Hello World!" << std::endl
     return 0;
 }
+*/
 
+// Run time error
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    7/0;
+    return 0;
+}
